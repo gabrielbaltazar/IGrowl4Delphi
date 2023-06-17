427e4f7b3fde40b6a4e0a94e6d2619662d77b7ab
Gabriel Baltazar <gabrielbaltazar.f1@gmail.com>
2023-06-17 12:57:33 -0300

    Record Types

--- Source/Growl4D.Types.pas
+++ Source/Growl4D.Types.pas
@@ -63,6 +63,149 @@ type
   TGrowlType = (gtInfo, gtSuccess, gtNotice, gtError, gtSimple, gtInfoSat,
     gtSuccessSat, gtNoticeSat, gtErrorSat);
 
+  TGrowlEnterAnimationHelper = record helper for TGrowlEnterAnimation
+  public
+    function ToString: string;
+  end;
+
+  TGrowlExitAnimationHelper = record helper for TGrowlExitAnimation
+  public
+    function ToString: string;
+  end;
+
+  TGrowlPositionHelper = record helper for TGrowlPosition
+  public
+    function ToString: string;
+  end;
+
+  TGrowlTypeHelper = record helper for TGrowlType
+  public
+    function ToString: string;
+  end;
+
 implementation
+
+{ TGrowlEnterAnimationHelper }
+
+function TGrowlEnterAnimationHelper.ToString: string;
+begin
+  Result := 'fadeIn';
+  case Self of
+    geaFadeInDown: Result := 'fadeInDown';
+    geaFadeIn: Result := 'fadeIn';
+    geaFadeInDownBig: Result := 'fadeInDownBig';
+    geaFadeInLeft: Result := 'fadeInLeft';
+    geaFadeInLeftBig: Result := 'fadeInLeftBig';
+    geaFadeInRight: Result := 'fadeInRight';
+    geaFadeInRightBig: Result := 'fadeInRightBig';
+    geaFadeInUp: Result := 'fadeInUp';
+    geaFadeInUpBig: Result := 'fadeInUpBig';
+    geaBounceIn: Result := 'bounceIn';
+    geaBounceInDown: Result := 'bounceInDown';
+    geaBounceInLeft: Result := 'bounceInLeft';
+    geaBounceInRight: Result := 'bounceInRight';
+    geaBounceInUp: Result := 'bounceInUp';
+    geaSlideInDown: Result := 'slideInDown';
+    geaSlideInLeft: Result := 'slideInLeft';
+    geaSlideInRight: Result := 'slideInRight';
+    geaSlideInUp: Result := 'slideInUp';
+    geaFlip: Result := 'flip';
+    geaFlipInX: Result := 'flipInX';
+    geaFlipInY: Result := 'flipInY';
+    geaLightSpeedIn: Result := 'lightSpeedIn';
+    geaRotateIn: Result := 'rotateIn';
+    geaRotateInDownLeft: Result := 'rotateInDownLeft';
+    geaRotateInDownRight: Result := 'rotateInDownRight';
+    geaRotateInUpLeft: Result := 'rotateInUpLeft';
+    geaRotateInUpRight: Result := 'rotateInUpRight';
+    geaBounce: Result := 'bounce';
+    geaFlash: Result := 'flash';
+    geaPulse: Result := 'pulse';
+    geaRubberBand: Result := 'rubberBand';
+    geaShake: Result := 'shake';
+    geaSwing: Result := 'swing';
+    geaTada: Result := 'tada';
+    geaWobble: Result := 'wobble';
+    geaRollIn: Result := 'rollIn';
+    geaZoomIn: Result := 'zoomIn';
+    geaZoomInDown: Result := 'zoomInDown';
+    geaZoomInLeft: Result := 'zoomInLeft';
+    geaZoomInRight: Result := 'zoomInRight';
+    geaZoomInUp: Result := 'zoomInUp';
+  end;
+end;
+
+{ TGrowlExitAnimationHelper }
+
+function TGrowlExitAnimationHelper.ToString: string;
+begin
+  Result := 'fadeOut';
+  case Self of
+    geaBounceOut: Result := 'bounceOut';
+    geaBounceOutDown: Result := 'bounceOutDown';
+    geaBounceOutLeft: Result := 'bounceOutLeft';
+    geaBounceOutRight: Result := 'bounceOutRight';
+    geaBounceOutUp: Result := 'bounceOutUp';
+    geaSlideOutDown: Result := 'slideOutDown';
+    geaSlideOutLeft: Result := 'slideOutLeft';
+    geaSlideOutRight: Result := 'slideOutRight';
+    geaSlideOutUp: Result := 'slideOutUp';
+    geaFadeOut: Result := 'fadeOut';
+    geaFadeOutDown: Result := 'fadeOutDown';
+    geaFadeOutDownBig: Result := 'fadeOutDownBig';
+    geaFadeOutLeft: Result := 'fadeOutLeft';
+    geaFadeOutLeftBig: Result := 'fadeOutLeftBig';
+    geaFadeOutRight: Result := 'fadeOutRight';
+    geaFadeOutRightBig: Result := 'fadeOutRightBig';
+    geaFadeOutUp: Result := 'fadeOutUp';
+    geaFadeOutUpBig: Result := 'fadeOutUpBig';
+    geaFlipOutX: Result := 'flipOutX';
+    geaFlipOutY: Result := 'flipOutY';
+    geaLightSpeedOut: Result := 'lightSpeedOut';
+    geaRotateOut: Result := 'rotateOut';
+    geaRotateOutDownLeft: Result := 'rotateOutDownLeft';
+    geaRotateOutDownRight: Result := 'rotateOutDownRight';
+    geaRotateOutUpLeft: Result := 'rotateOutUpLeft';
+    geaRotateOutUpRight: Result := 'rotateOutUpRight';
+    geaHinge: Result := 'hinge';
+    geaRollOut: Result := 'rollOut';
+    geaZoomOut: Result := 'zoomOut';
+    geaZoomOutDown: Result := 'zoomOutDown';
+    geaZoomOutLeft: Result := 'zoomOutLeft';
+    geaZoomOutRight: Result := 'zoomOutRight';
+    geaZoomOutUp: Result := 'zoomOutUp';
+  end;
+end;
+
+{ TGrowlPositionHelper }
+
+function TGrowlPositionHelper.ToString: string;
+begin
+  Result := 'center';
+  case Self of
+    gpLeft: Result := 'left';
+    gpCenter: Result := 'center';
+    gpRight: Result := 'right';
+    gpTop: Result := 'top';
+    gpBottom: Result := 'bottom';
+  end;
+end;
+
+{ TGrowlTypeHelper }
+
+function TGrowlTypeHelper.ToString: string;
+begin
+  case Self of
+    gtInfo: Result := 'info';
+    gtSuccess: Result := 'success';
+    gtNotice: Result := 'notice';
+    gtError: Result := 'error';
+    gtSimple: Result := 'simple';
+    gtInfoSat: Result := 'info_sat';
+    gtSuccessSat: Result := 'success_sat';
+    gtNoticeSat: Result := 'notice_sat';
+    gtErrorSat: Result := 'error_sat';
+  end;
+end;
 
 end.
